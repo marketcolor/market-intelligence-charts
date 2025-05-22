6b6a7c399ac29eaee4d91ced844a125be80f4376
marketcolor <golle404@gmail.com>
2025-05-22 17:36:57 +0200

    protected all routes

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,11 @@
 import { clerkMiddleware, createRouteMatcher } from '@clerk/astro/server'
 
-const isProtectedRoute = createRouteMatcher(['/labor-market(.*)'])
+const isPublicRoute = createRouteMatcher(['/sign-in(.*)'])
 
 export const onRequest = clerkMiddleware((auth, context) => {
-	const { redirectToSignIn, userId } = auth()
-	console.log('hitting middleware')
-	console.log('user id: ', userId)
-	console.log(isProtectedRoute(context.request))
+	const { userId } = auth()
 
-	if (!userId && isProtectedRoute(context.request)) {
-		console.log('redirect to sign in')
+	if (!userId && !isPublicRoute(context.request)) {
 		return Response.redirect(new URL('/sign-in', context.url), 302)
-		// return redirectToSignIn()
 	}
 })
